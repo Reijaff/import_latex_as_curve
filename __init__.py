c699829e6da3983acb7366bd200a9550fbeef60a
Reijaff <interlord406@gmail.com>
2023-11-12 13:23:48 +0200

    update

--- __init__.py
+++ __init__.py
@@ -4,11 +4,13 @@ from . import import_latex_as_curve
 
 bl_info = {
     "name": "import_latex_as_curve",
-    "author": "Reijaff",
-    "version": (0, 0, 1),
-    "blender": (2, 8, 0),
+    "author": "reijaff",
+    "version": (0, 1, 0),
+    "blender": (3, 4, 0),
     "location": "",
+    "description": "render latex as curve",
     "warning": "",
+    "doc_url": "https://example.com",
     "category": "3D View"}
 
 
